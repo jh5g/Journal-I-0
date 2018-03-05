e5a5f98521c2039e89cb33ec083b68d1fe49a6a0
jh5g <31213220+jh5g@users.noreply.github.com>
2018-03-05 20:11:21 +0000

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewEntry = new System.Windows.Forms.RichTextBox();
+            this.ExistingJournal = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateEntry = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NewEntry
+            // 
+            this.NewEntry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NewEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewEntry.Location = new System.Drawing.Point(150, 66);
+            this.NewEntry.Name = "NewEntry";
+            this.NewEntry.Size = new System.Drawing.Size(1418, 382);
+            this.NewEntry.TabIndex = 0;
+            this.NewEntry.Text = "";
+            this.NewEntry.TextChanged += new System.EventHandler(this.NewEntry_TextChanged);
+            // 
+            // ExistingJournal
+            // 
+            this.ExistingJournal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExistingJournal.Enabled = false;
+            this.ExistingJournal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExistingJournal.Location = new System.Drawing.Point(126, 481);
+            this.ExistingJournal.Name = "ExistingJournal";
+            this.ExistingJournal.Size = new System.Drawing.Size(1479, 625);
+            this.ExistingJournal.TabIndex = 1;
+            this.ExistingJournal.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1626, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(267, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(267, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(267, 30);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(267, 30);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // dateEntry
+            // 
+            this.dateEntry.Location = new System.Drawing.Point(12, 89);
+            this.dateEntry.Name = "dateEntry";
+            this.dateEntry.Size = new System.Drawing.Size(131, 43);
+            this.dateEntry.TabIndex = 3;
+            this.dateEntry.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Location = new System.Drawing.Point(13, 66);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 25);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "Date";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(13, 207);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(100, 99);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 607);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1626, 1142);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.dateEntry);
+            this.Controls.Add(this.ExistingJournal);
+            this.Controls.Add(this.NewEntry);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Jounral I/0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox NewEntry;
+        private System.Windows.Forms.RichTextBox ExistingJournal;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox dateEntry;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Main : Form
+    {
+        static string fileName = "Untitled";
+        Dictionary<string, string> diaryContents = new Dictionary<string, string>();
+        public Main()
+        {
+            InitializeComponent();
+            this.Text = "Journal I/O - " + fileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateEntry.Text = DateTime.UtcNow.Date.ToString("dd/MM/yyyy");
+        }
+        public void formatNewEntry()
+        {
+            var entry =  NewEntry.Text;
+            string date = dateEntry.Text;
+            diaryContents.Add(date, entry);
+            //var b = "";
+        }
+        public string formatJournal()//to be saved
+        {
+            string toReturn = "";
+            List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+            List<DateTime> datesList = new List<DateTime>();
+            foreach (KeyValuePair<string, string> kvp in diaryContents)
+            {
+                datesList.Add(Convert.ToDateTime(kvp.Key));
+            }
+            var diaryContentsSorted = diaryContents.Keys.OrderByDescending(d => d);
+            foreach (string a in diaryContentsSorted)
+            {
+                pairs.Add(new KeyValuePair<string, string>(a, diaryContents[a]));
+            }
+            foreach(KeyValuePair<string, string> kvp in pairs)
+            {
+                string key = kvp.Key;
+                string value = kvp.Value;
+                toReturn = toReturn + "{" + key + ":" + value + "}";
+            }
+            return toReturn;
+        }
+        public string formatJournalString(string unformatted) //to be displayed based off of string from file
+        {
+            Dictionary<string, string> toReturnDict = new Dictionary<string, string>();
+            string pat = "{(.*?):(.*?)}";
+            Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+            Match m = r.Match(unformatted);
+            while (m.Success)
+            {
+                string key = "";
+                string value = "";
+                for (int i = 1; i <= 2; i++)
+                {
+                    Group g = m.Groups[i];
+                    if (i == 1)
+                    {
+                        key = g.Value;
+                    }
+                    else if (i == 2)
+                    {
+                        value = g.Value;
+                    }
+                }
+                toReturnDict.Add(key, value);
+                m = m.NextMatch();
+            }
+            return formatJournalOut(toReturnDict);
+        }
+        public string formatJournalOut(Dictionary<string, string> unformatted) //to be displayed from dictionary
+        {
+            return JsonConvert.SerializeObject(unformatted, Formatting.Indented);
+        }
+        public Dictionary<string, string> formatJournalDict (string unformatted)// to be turned into dictionary from file 
+        {
+            Dictionary<string, string> toReturn = new Dictionary<string,string>();
+            string pat = "{(.*?):(.*?)}"; 
+            Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+            Match m = r.Match(unformatted);
+            while (m.Success)
+            {
+                string key = "";
+                string value = "";
+                for (int i = 1; i <= 2; i++)
+                {
+                    Group g = m.Groups[i];
+                    if (i == 1)
+                    {
+                        key = g.Value;
+                    }
+                    else if (i == 2)
+                    {
+                        value = g.Value;
+                    }
+                }
+                toReturn.Add(key, value);
+                m = m.NextMatch();
+            }
+            return toReturn;
+        }
+        public void SaveAs()
+        {
+            string Contents = formatJournal();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "Journal I/O File (*.jour)|*.jour"; //txt files (*.txt)|*.txt|All files (*.*)|*.*
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog1.FileName.ToString());
+                fileName = saveFileDialog1.FileName.ToString();
+                using (StringReader reader = new StringReader(Contents))
+                {
+                    string line = string.Empty;
+                    do
+                    {
+                        line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            file.WriteLine(line);
+                        }
+
+                    } while (line != null);
+                }
+                file.Close();
+                this.Text = "Journal I/O - " + fileName;
+            }
+
+        }
+        public void Save()
+        {
+            if (fileName != "Untitled")
+            {
+                string Contents = formatJournal();
+                StreamWriter file = new StreamWriter(fileName);
+                using (StringReader reader = new StringReader(Contents))
+                {
+                    string line = string.Empty;
+                    do
+                    {
+                        line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            file.WriteLine(line);
+                        }
+
+                    } while (line != null);
+                }
+                file.Close();
+                this.Text = "Journal I/O - " + fileName;
+            }
+            else
+            {
+                SaveAs();
+            }
+        }
+        private void Open()
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "Journal I/O File (*.jour)|*.jour";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName.ToString();
+                
+                ExistingJournal.Text = formatJournalString(File.ReadAllText(fileName));
+                NewEntry.Text = "";
+                this.Text = "Journal I/O " + fileName;
+                diaryContents = formatJournalDict(File.ReadAllText(fileName));
+            }
+            
+
+        }
+        public void New()
+        {
+            NewEntry.Text = "";
+            ExistingJournal.Text = "";
+            diaryContents = new Dictionary<string, string>();
+            fileName = "Untitled";
+            this.Text = "Journal I/O - " + fileName;
+        }
+        public void Add()
+        {
+            formatNewEntry();
+            ExistingJournal.Text = formatJournalOut(diaryContents);
+            //var b = "";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void NewEntry_TextChanged(object sender, EventArgs e)
+        {
+            this.Text = "Journal I/O - " + fileName + "*";
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            New();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.Text.Substring(this.Text.Length - 1) == "*")
+            {
+                DialogResult saveQuery = MessageBox.Show(fileName + " is has not been saved.\nDo you want to save it?", "Unsaved Work", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+                if (saveQuery == DialogResult.Yes)
+                {
+                    e.Cancel = true;
+                    Save();
+                    this.Close();
+                }
+                else if (saveQuery == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+
+            }
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (diaryContents.ContainsKey(dateEntry.Text))
+            {
+                DialogResult overwriteQuery = MessageBox.Show("This date already has an entry\n Pressing Ok will overwrite", "Existing entry", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+                if (overwriteQuery == DialogResult.OK)
+                {
+                    diaryContents.Remove(dateEntry.Text);
+                    Add();
+                }
+            }
+            else
+            {
+                Add();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                ExistingJournal.Enabled = true;
+            }
+            else if (checkBox1.Checked == false)
+            {
+                ExistingJournal.Enabled = false;
+            }
+        }
+    }
+}
