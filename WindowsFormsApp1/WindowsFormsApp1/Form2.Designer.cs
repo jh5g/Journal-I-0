e0fac2f0636037444336697edd5aab31be0b8213
jh5g <31213220+jh5g@users.noreply.github.com>
2018-04-05 17:46:24 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace JournalIO
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.DateEntry = new System.Windows.Forms.RichTextBox();
+            this.ContentsEntry = new System.Windows.Forms.RichTextBox();
+            this.Results = new System.Windows.Forms.RichTextBox();
+            this.Next = new System.Windows.Forms.Button();
+            this.NumResults = new System.Windows.Forms.TextBox();
+            this.StartSearch = new System.Windows.Forms.Button();
+            this.DateDisplayer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(36, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Search by date below:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(475, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Tag = "";
+            this.textBox2.Text = "Search by contents below:";
+            // 
+            // DateEntry
+            // 
+            this.DateEntry.Location = new System.Drawing.Point(36, 111);
+            this.DateEntry.Name = "DateEntry";
+            this.DateEntry.Size = new System.Drawing.Size(367, 60);
+            this.DateEntry.TabIndex = 2;
+            this.DateEntry.Text = "";
+            // 
+            // ContentsEntry
+            // 
+            this.ContentsEntry.Location = new System.Drawing.Point(470, 111);
+            this.ContentsEntry.Name = "ContentsEntry";
+            this.ContentsEntry.Size = new System.Drawing.Size(291, 59);
+            this.ContentsEntry.TabIndex = 3;
+            this.ContentsEntry.Text = "";
+            // 
+            // Results
+            // 
+            this.Results.Location = new System.Drawing.Point(45, 287);
+            this.Results.Name = "Results";
+            this.Results.ReadOnly = true;
+            this.Results.Size = new System.Drawing.Size(897, 339);
+            this.Results.TabIndex = 4;
+            this.Results.Text = "";
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(782, 222);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(141, 46);
+            this.Next.TabIndex = 5;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // NumResults
+            // 
+            this.NumResults.Location = new System.Drawing.Point(579, 232);
+            this.NumResults.Name = "NumResults";
+            this.NumResults.Size = new System.Drawing.Size(148, 26);
+            this.NumResults.TabIndex = 6;
+            this.NumResults.Text = "Result";
+            // 
+            // StartSearch
+            // 
+            this.StartSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartSearch.Location = new System.Drawing.Point(842, 89);
+            this.StartSearch.Name = "StartSearch";
+            this.StartSearch.Size = new System.Drawing.Size(193, 100);
+            this.StartSearch.TabIndex = 7;
+            this.StartSearch.Text = "Search";
+            this.StartSearch.UseVisualStyleBackColor = true;
+            this.StartSearch.Click += new System.EventHandler(this.StartSearch_Click);
+            // 
+            // DateDisplayer
+            // 
+            this.DateDisplayer.Location = new System.Drawing.Point(57, 232);
+            this.DateDisplayer.Name = "DateDisplayer";
+            this.DateDisplayer.Size = new System.Drawing.Size(141, 26);
+            this.DateDisplayer.TabIndex = 8;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 704);
+            this.Controls.Add(this.DateDisplayer);
+            this.Controls.Add(this.StartSearch);
+            this.Controls.Add(this.NumResults);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.ContentsEntry);
+            this.Controls.Add(this.DateEntry);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox DateEntry;
+        private System.Windows.Forms.RichTextBox ContentsEntry;
+        private System.Windows.Forms.RichTextBox Results;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.TextBox NumResults;
+        private System.Windows.Forms.Button StartSearch;
+        private System.Windows.Forms.TextBox DateDisplayer;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JournalIO
+{
+    public partial class Form2 : Form
+    {
+        public Dictionary<string, string> diaryContents { get; set; }
+        List<KeyValuePair<string, string>> contentsList { get; set; }
+        //Dictionary<string, string> reultsDictionary { get; set; }
+        int currResult = 0;
+        int noResults = 0;
+        public Form2()
+        {
+            InitializeComponent();
+            this.diaryContents = new Dictionary<string, string>();
+            this.contentsList = new List<KeyValuePair<string, string>>();
+            //this.reultsDictionary = new Dictionary<string, string>();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        }
+        private void StartSearch_Click(object sender, EventArgs e)
+        {
+            string date = DateEntry.Text;
+            string contents = ContentsEntry.Text;
+            //MessageBox.Show(DateEntry.Text);
+            if (date != "")
+            {
+                foreach (KeyValuePair<string, string> kvp in this.diaryContents)
+                {
+                    if (kvp.Key.Contains(date))
+                    {
+                        this.contentsList.Add(kvp);
+                    }
+                }
+            }
+            else if (contents != "")
+            {
+                foreach(KeyValuePair<string, string> kvp in this.diaryContents)
+                {
+                    if (kvp.Value.Contains(contents))
+                    {
+                        this.contentsList.Add(kvp);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No search method was found", "Search method", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (contentsList.Any())
+            {
+                //reultsDictionary = contentsList.ToDictionary(pair => pair.Key, pair => pair.Value);
+                //Results.Text = JsonConvert.SerializeObject(reultsDictionary, Formatting.Indented);
+                noResults = contentsList.Count();
+                currResult = 1;
+                this.NumResults.Text = currResult.ToString();
+                this.NumResults.Text += " / ";
+                this.NumResults.Text += noResults.ToString();
+                this.Results.Text = contentsList[0].Value;
+                this.DateDisplayer.Text = contentsList[0].Key;
+                
+            }
+            else
+            {
+                MessageBox.Show("No results were found", "No Results", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        } //fills list with kvps then if possible displays first result 
+
+        private void Next_Click(object sender, EventArgs e) 
+        {
+            if (contentsList.Any())
+            {
+                if (contentsList.Any())
+                {
+                    if (noResults >= currResult + 1) //noResults >= currResult + 1 
+                    {
+                        //if (currResult -1 < contentsList.)
+                        currResult += 1;
+                        this.NumResults.Text = currResult.ToString();
+                        this.NumResults.Text += " / ";
+                        this.NumResults.Text += noResults.ToString();
+                        this.Results.Text = contentsList[currResult - 1].Value;
+                        this.DateDisplayer.Text = contentsList[currResult - 1].Key;
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
